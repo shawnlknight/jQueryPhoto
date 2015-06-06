5242532d7f1a7081a1f7a70217b0b827f6317c10
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2015-06-06 12:05:27 -0400

    Made improvements in js, css, and html. A little responsiveness too.

--- js/site.js
+++ js/site.js
@@ -1,63 +1,61 @@
 $(document).ready(function() {
 
 
-//callback for click events on page to load hidden divs
-var showAlbum = function(event) {
+	//callback for click events on page to load hidden divs
+	var showAlbum = function(event) {
       event.preventDefault();
-
       var contentDiv = $(this).attr("rel");
-
       console.log($("." + contentDiv));
 
-      if($("." + contentDiv).hasClass("show")) {
-        return;
-      } else {
-      $("." + contentDiv).siblings().removeClass("show");
-      $("." + contentDiv).addClass("show");
-      }
+		if ($("." + contentDiv).hasClass("show")) {
+			return;
+		}  else {
+			$("." + contentDiv).siblings().removeClass("show");
+			$("." + contentDiv).addClass("show");
+		}
   }
 
-//when clicking on nav link
-//show related page 
-$(".nav a").click(showAlbum);
+	/* When clicking on nav link,
+		show related page 
+	*/
+	$(".nav a").click(showAlbum);
 
+	//scrolling through album pictures
+	// $(".album1 img").click(function(event) {
+	// 	event.preventDefault();
+	// 	var $thisPicture = $(this);
 
+	// 	$thisPicture.closest("li").siblings().toggle();
+	// 	$thisPicture.toggleClass("magnify");
+	// });
 
-//scrolling through album pictures
-$(".album1 img").click(function(event) {
-      event.preventDefault();
-      var $thisPicture = $(this);
+	// $(".album2 img").click(function(event) {
+	// 	event.preventDefault();
+	// 	var $thisPicture = $(this);
 
-      // console.log($(this));
+	// 	$thisPicture.closest("li").siblings().toggle();
+	// 	$thisPicture.toggleClass("magnify");
+	// });
 
-$thisPicture.closest("li").siblings().toggle();
-$thisPicture.toggleClass("magnify");
-});
+	// $(".album3 img").click(function(event) {
+	// 	event.preventDefault();
+	// 	var $thisPicture = $(this);
 
+	// 	$thisPicture.closest("li").siblings().toggle();
+	// 	$thisPicture.toggleClass("magnify");
+	// });
 
+	/*
+		 Left the above code commented out to show
+		 how it can be condensed to these few lines of code
+	 */
+	$(".picAlbum").on("click", "img", function(event) {
+		event.preventDefault();
+		var $thisPicture = $(this);
 
-$(".album2 img").click(function(event) {
-      event.preventDefault();
-      var $thisPicture = $(this);
-
-      // console.log($(this));
-
-$thisPicture.closest("li").siblings().toggle();
-$thisPicture.toggleClass("magnify");
-});
-
-
-$(".album3 img").click(function(event) {
-      event.preventDefault();
-      var $thisPicture = $(this);
-
-      // console.log($(this));
-
-$thisPicture.closest("li").siblings().toggle();
-$thisPicture.toggleClass("magnify");
-});
-
-
+		$thisPicture.closest("li").siblings().toggle();
+		$thisPicture.toggleClass("magnify");
+	});
 });
 
 
